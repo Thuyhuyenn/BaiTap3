7b34043256276909be207076e0e62e468fb07d20
Thuyhuyenn <nguyenthithuyhuyen905@gmail.com>
2025-09-27 03:51:54 +0700

    Improve Register UI

--- app/src/main/java/com/example/btcn/LoginActivity.java
+++ app/src/main/java/com/example/btcn/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.btcn;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,14 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        TextView btn=findViewById(R.id.textViewSignUp);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+            }
         });
+
+        ;
     }
 }
